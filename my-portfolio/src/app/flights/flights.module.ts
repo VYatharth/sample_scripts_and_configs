ccffa334dada3da12b8bf5269db351dd422ff197
Yathartha <varshneya.yathartha@gmail.com>
2023-09-27 10:47:13 +0530

    Added flights module

---
+++ my-portfolio/src/app/flights/flights.module.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FlightsRoutingModule } from './flights-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FlightsRoutingModule
+  ],
+  declarations: [
+  ]
+})
+export class FlightsModule { }
