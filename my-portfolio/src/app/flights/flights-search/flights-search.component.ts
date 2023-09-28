a71a7b4fcc2ab58e759fdebae8b2d18a93fb50a8
Yathartha <varshneya.yathartha@gmail.com>
2023-09-28 13:47:59 +0530

    Added flights search component

---
+++ my-portfolio/src/app/flights/flights-search/flights-search.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+
+@Component({
+  selector: 'app-flights-search',
+  templateUrl: './flights-search.component.html'
+})
+export class FlightsSearchComponent {
+  constructor() {
+  }
+}
