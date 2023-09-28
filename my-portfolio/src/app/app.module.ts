54437ad9113b9a51a47b2bb52ccc86b5adeafdbf
Yathartha <varshneya.yathartha@gmail.com>
2023-09-28 13:49:05 +0530

    Updated app module and component

--- my-portfolio/src/app/app.module.ts
+++ my-portfolio/src/app/app.module.ts
@@ -3,13 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FlightsModule } from './flights/flights.module';
+import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
+    HomeComponent,
     AppComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    FlightsModule,
     AppRoutingModule
   ],
   providers: [],
